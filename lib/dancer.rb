8119fc7328afc9809178abb4123eab1dfffc7bc2
Bobby Neal <rsneal01@gmail.com>
2020-06-24 17:47:10 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -5,7 +5,8 @@ require_relative './fancy_dance.rb'
 
 
     class Dancer
-      include Dance
+       extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
      
       attr_accessor :name
      
