7c24bfd04776356394d9e2ec8128294956f25801
Bobby Neal <rsneal01@gmail.com>
2020-06-24 17:44:35 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,13 +1,7 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
-    class Kid
-      include Dance
-      extend MetaDancing
-     
-      attr_accessor :name
-     
-      def initialize(name)
-        @name = name
-      end
+        class Kid
+      extend FancyDance::ClassMethods
+      include FancyDance::InstanceMethods
     end
